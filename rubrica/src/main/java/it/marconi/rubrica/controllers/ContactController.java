67ee2eed3bb009ced76e467c8e2b5c0baed384ea
Angela Trentin <angela.trentin@mat.tn.it>
2026-03-30 14:31:49 +0000

    feat: aggiunta della validazione dei campi nel modulo di contatto e gestione degli errori; aggiornamento delle viste per il feedback utente

--- rubrica/src/main/java/it/marconi/rubrica/controllers/ContactController.java
+++ rubrica/src/main/java/it/marconi/rubrica/controllers/ContactController.java
@@ -7,16 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.marconi.rubrica.domain.Contact;
 import it.marconi.rubrica.domain.ContactForm;
 import it.marconi.rubrica.services.ContactService;
+import jakarta.validation.Valid;
 
 @Controller
 public class ContactController {
@@ -36,16 +39,30 @@ public class ContactController {
         // passo un'istanza vuota di form alla pagina web
         return new ModelAndView("contact-form").addObject(new ContactForm());
     }
-    
+
+    //endpoint per la richiesta POST, deve  salvare il contatto nel DB
     @PostMapping("/new")
-    public ModelAndView handleNewContact(@ModelAttribute ContactForm contactForm) {
+    public ModelAndView handleNewContact(
+        @ModelAttribute @Valid ContactForm contactForm,
+        BindingResult br,
+        RedirectAttributes attr
+        //esito della validazione (subito dopo parametro da validare)
+    ) {
+        // controllo esito della validazione
+        if (br.hasErrors())
+            return new ModelAndView("contact-form");
+
+        attr.addFlashAttribute("newContact", true);
 
        Contact c = contactService.save(contactForm);
+
+       //aggiungere un parametro speciale che sopravviva al redirect
 
     //    return new ModelAndView("contact-form");
         return new ModelAndView("redirect:/contact?id=" + c.getId());
     }
 
+    //pattern PRG
     @GetMapping(path = "contact", params = "id")
     public ModelAndView showContact(@RequestParam("id") UUID contactId) {
 
--- rubrica/src/main/java/it/marconi/rubrica/domain/ContactForm.java
+++ rubrica/src/main/java/it/marconi/rubrica/domain/ContactForm.java
@@ -1,5 +1,9 @@
 package it.marconi.rubrica.domain;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +12,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class ContactForm {
+
+    //inserisco i controlli di validazione per ogni campo (causa di @Valid)
+    @NotEmpty
+    @Size(max=30)
     private String name;
+
+    @NotEmpty
+    @Size(max=50)
     private String surname;
+
+    @Pattern (regexp = "([0-9]{10})") 
     private String phoneNumber;
+
+    @Email
     private String email;
 
     
